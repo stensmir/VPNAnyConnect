8cdac329ba194aee89f2ff1f08434180065e92e0
Yuriy Durnev <stensmir@gmail.com>
2022-01-22 22:48:06 +0300

    Initial Commit

---
+++ Cisco/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cisco
+//
+//  Created by Юрий Дурнев on 22.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
