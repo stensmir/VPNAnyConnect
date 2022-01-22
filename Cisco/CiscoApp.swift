76864dea01e4f0b859c1f36d38ae686940e81b62
Yuriy Durnev <stensmir@gmail.com>
2022-01-22 22:48:06 +0300

    Initial Commit

---
+++ Cisco/CiscoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiscoApp.swift
+//  Cisco
+//
+//  Created by Юрий Дурнев on 22.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CiscoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
